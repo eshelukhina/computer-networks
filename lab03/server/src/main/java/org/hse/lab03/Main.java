4b4531d55e4fb4ad7c8370a2ee92fb5210fc9e6a
Shelukhina Ekaterina Andreevna <shelukchina2447@gmail.com>
2023-03-09 00:21:12 +0300

    Solved Task D

--- lab03/server/src/main/java/org/hse/lab03/Main.java
+++ lab03/server/src/main/java/org/hse/lab03/Main.java
@@ -2,7 +2,13 @@ package org.hse.lab03;
 
 public class Main {
     public static void main(String[] args) {
-        MultithreadedServer server = new MultithreadedServer();
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Must be only one argument");
+        }
+
+        Integer concurrencyLevel = Integer.parseInt(args[0]);
+
+        MultithreadedServer server = new MultithreadedServer(concurrencyLevel);
         server.run();
     }
 }
